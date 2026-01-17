d2662c8ee55d930847fd94b8fc3460f7af7fc911
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2026-01-17 19:55:57 +0530

    Create sitemap.js

---
+++ app/sitemap.js
@@ -0,0 +1,12 @@
+export const dynamic = 'force-static'
+
+export default function sitemap() {
+  return [
+    {
+      url: 'https://priyanshu.is-a.dev',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+  ]
+}
